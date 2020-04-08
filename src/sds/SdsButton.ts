533f5f840b98e3c0700eab8bd4dc5f13e240a8f4
Jan Miksovsky <jmiksovsky@salesforce.com>
2020-04-08 11:33:21 -0700

    Allow variant to be changed dynamically.

--- src/sds/SdsButton.ts
+++ src/sds/SdsButton.ts
@@ -25,8 +25,19 @@ export default class SdsButton extends Button {
     }
     // Append variant class, if attribute exist
     if (changed.variant) {
-      const computedClassName = `lwc-button_${this[internal.state].variant}`;
-      this[internal.ids].inner.classList.add(computedClassName);
+      // Remove all existing variant classes.
+      const inner = this[internal.ids].inner;
+      const variantClass = `lwc-button_${this[internal.state].variant}`;
+      inner.classList.forEach(existingClass => {
+        if (
+          existingClass.startsWith("lwc-button_") &&
+          existingClass !== variantClass
+        ) {
+          inner.classList.remove(existingClass);
+        }
+      });
+      // Add the new variant class.
+      inner.classList.add(variantClass);
     }
   }
 
